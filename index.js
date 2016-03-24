0cdd94ff1030e72e73cd2eaab52f30c8a559d73f
audreyandoy <audreylandoy@gmail.com>
2016-03-24 13:23:01 -0700

    merged model and controllers

--- index.js
+++ index.js
@@ -3,4 +3,20 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var expressJWT = require('express-jwt');
 var jwt = require('jsonwebtoken');
+var bcrypt = require
 var app = express();
+
+
+
+var mongoose = require('mongoose');
+var User = require('./models/user');
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/api/users', require('./controllers/user'));
+app.use('/api/bags', require('./controllers/bag'));
+
+
+
+
+app.listen(3000);
