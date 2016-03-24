5ede25df67bfa0631179601c5e5a4c249a827b4a
audreyandoy <audreylandoy@gmail.com>
2016-03-24 11:42:27 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var expressJWT = require('express-jwt');
+var jwt = require('jsonwebtoken');
+var app = express();
